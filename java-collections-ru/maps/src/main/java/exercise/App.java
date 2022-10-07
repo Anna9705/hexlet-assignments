74028501b48d43e2c174461608bb5c942ce2513f
@hexlet/cli <support@hexlet.io>
2022-10-07 12:58:08 +0300

    submit java-collections-ru/maps

---
+++ java-collections-ru/maps/src/main/java/exercise/App.java
@@ -0,0 +1,46 @@
+package exercise;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// BEGIN
+public class App {
+
+   public static void main(String[] args) {
+       String sentence1 = "word text cat apple word map apple word";
+       System.out.println(App.getWordCount(sentence1));
+       System.out.println(toString(App.getWordCount(sentence1)));
+       System.out.println(toString(App.getWordCount(sentence1)).length());
+   }
+
+    public static Map<String, Integer> getWordCount(String sentence) {
+        if (sentence.equals("")) {
+            return new HashMap<>();
+        }
+        String[] words = sentence.split(" ");
+        Map<String, Integer> wordsMap = new HashMap<>();
+        for (String word : words) {
+            if (wordsMap.containsKey(word)) {
+                int currentValue = wordsMap.get(word);
+                wordsMap.put(word, currentValue + 1);
+            } else {
+                wordsMap.put(word, 1);
+            }
+        }
+        return wordsMap;
+    }
+
+    public static String toString(Map<String, Integer> wordsMap) {
+       if (wordsMap.isEmpty()) {
+           return "{}";
+       }
+       StringBuilder result = new StringBuilder();
+        result.append("{\n");
+        for (Map.Entry<String, Integer> word : wordsMap.entrySet()) {
+            result.append("  ").append(word.getKey()).append(": ").append(word.getValue()).append("\n");
+        }
+        result.append("}");
+        return result.toString();
+    }
+}
+//END
