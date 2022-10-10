0b461d9d54b645d02a457891f58da1144c31bfb3
@hexlet/cli <support@hexlet.io>
2022-10-10 13:09:21 +0300

    submit java-collections-ru/tests

--- java-collections-ru/tests/src/test/java/exercise/AppTest.java
+++ java-collections-ru/tests/src/test/java/exercise/AppTest.java
@@ -12,25 +12,23 @@ class AppTest {
     void testTake() {
         // BEGIN
         List<Integer> numbers1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
-
-        int actualSize1 = App.take(numbers1, 3).size();
-        assertThat(actualSize1).isEqualTo(3);
-
-        List<Integer> actualList1 = App.take(numbers1, 3);
-        assertThat(actualList1).isEqualTo(Arrays.asList(1, 2, 3));
+        List<Integer> actual1 = App.take(numbers1, 3);
+        List<Integer> expected1 = Arrays.asList(1, 2, 3);
+        assertThat(actual1).isEqualTo(expected1);
 
 
         List<Integer> numbers2 = new ArrayList<>(Arrays.asList(5, 15, 18, 19));
-
-        int actualSize2 = App.take(numbers2, 10).size();
-        assertThat(actualSize2).isEqualTo(4);
-
-        List<Integer> actualList2 = App.take(numbers2, 10);
-        assertThat(actualList2).isEqualTo(Arrays.asList(5, 15, 18, 19));
+        List<Integer> actual2 = App.take(numbers2, 10);
+        List<Integer> expected2 = Arrays.asList(5, 15, 18, 19);
+        assertThat(actual2).isEqualTo(expected2);
 
         List<Integer> numbers3 = new ArrayList<>(Arrays.asList(5, 15, 18, 19));
         List<Integer> actualList3 = App.take(numbers3, 0);
         assertThat(actualList3).isEmpty();
+
+        List<Integer> numbers4 = new ArrayList<>();
+        List<Integer> actual4 = App.take(numbers4, 5);
+        assertThat(actual4).isEmpty();
         // END
     }
 }
